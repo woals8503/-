b11acfe2775deb72f14d1d6e58ad471957908a5c
이지환 <roovies55@gmail.com>
2023-05-22 14:11:34 +0900

    INIT : 초기 프로젝트

---
+++ src/main/java/com/oneline/shimpyo/ShimpyoApplication.java
@@ -0,0 +1,13 @@
+package com.oneline.shimpyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShimpyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShimpyoApplication.class, args);
+	}
+
+}
