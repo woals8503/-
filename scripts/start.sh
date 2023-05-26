0239f891ab07b61eb840fbe9373506e51b35fba1
이지환 <roovies77@gmail.com>
2023-05-26 14:32:39 +0900

    appspec.yml hooks 추가, start.sh nohup수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -29,8 +29,5 @@ echo "> $JAR_NAME 실행"
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
-nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ubuntu/application-oauth.properties,/home/ubuntu/application-real-db.properties \
-    -Dspring.profiles.active=$IDLE_PROFILE \
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup sudo nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &
 
