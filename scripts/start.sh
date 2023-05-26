3624216bbd241e753e219fd029a49fe8d9aa8844
이지환 <roovies77@gmail.com>
2023-05-26 14:48:33 +0900

    start.sh 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -29,5 +29,5 @@ echo "> $JAR_NAME 실행"
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
-sudo nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &
 
+nohup java -jar /home/ubuntu/action/shimpyo-0.0.1-SNAPSHOT.jar
