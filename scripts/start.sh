739cacb05d4cc3c6b2d10b3830b3e7522d8682e9
이지환 <roovies77@gmail.com>
2023-05-26 15:05:18 +0900

    start.sh 현재 구동중인 애플리케이션 삭제 쉘코드 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -15,6 +15,20 @@ echo "> cp $REPOSITORY/deploy/*.jar $REPOSITORY/"
 
 cp $REPOSITORY/deploy/*.jar $REPOSITORY/
 
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
+
+echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -9 $CURRENT_PID"
+    kill -9 $CURRENT_PID
+    sleep 2
+fi
+
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
