42d2e847b120f877de016f732ff5fba2dcf54140
blackarea <supersute142@gmail.com>
2023-05-28 15:04:03 +0900

    통신용 테스트 Json 형식

--- src/main/java/com/oneline/shimpyo/controller/TestController.java
+++ src/main/java/com/oneline/shimpyo/controller/TestController.java
@@ -1,24 +1,33 @@
 package com.oneline.shimpyo.controller;
 
-import org.springframework.stereotype.Controller;
+import com.oneline.shimpyo.domain.BaseResponse;
+import com.oneline.shimpyo.domain.dto.SampleDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class TestController {
 
+    @GetMapping("/test")
+    public BaseResponse<SampleDto> sample(){
+        return new BaseResponse<>(new SampleDto("first JSON Test Data", "second JSON Test Data"));
+    }
+
     @GetMapping("/test/data")
-    public String testMethod(){
+    public String testMethod() {
         return "Completed??";
     }
 
     @GetMapping("/test/data3")
-    public String test3Method(){
+    public String test3Method() {
         return "테스트 성공!";
     }
 
     @GetMapping("/test/woals")
-    public String test3Method2(){
+    public String test3Method2() {
         return "woals";
     }
 
---
+++ src/main/java/com/oneline/shimpyo/domain/dto/SampleDto.java
@@ -0,0 +1,14 @@
+package com.oneline.shimpyo.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SampleDto {
+
+    private String firstParam;
+    private String secondParam;
+}
