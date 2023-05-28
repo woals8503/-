9eb7196eb8b07ac75dcf6a83eca20e9c1fb5d158
이지환 <roovies77@gmail.com>
2023-05-28 16:50:52 +0900

    CORS 오류 - CrossOrigin 추가

--- src/main/java/com/oneline/shimpyo/controller/TestController.java
+++ src/main/java/com/oneline/shimpyo/controller/TestController.java
@@ -2,12 +2,14 @@ package com.oneline.shimpyo.controller;
 
 import com.oneline.shimpyo.domain.BaseResponse;
 import com.oneline.shimpyo.domain.dto.SampleDto;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class TestController {
 
