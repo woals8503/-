263683bed30afbd00522a64ad26ac55cc483071d
blackarea <supersute142@gmail.com>
2023-06-02 23:08:49 +0900

    CI/CD 오류 해결시도10

--- scripts/start.sh
+++ scripts/start.sh
@@ -63,6 +63,9 @@ echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
         -Dspring.config.location=classpath:/application.yml,/home/ubuntu/action/application-db.yml \
         /home/ubuntu/action/shimpyo-0.0.1-SNAPSHOT.jar > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar \
+    -Dspring.config.location=/home/ubuntu/action/application-db.yml \
+    /home/ubuntu/action/shimpyo-0.0.1-SNAPSHOT.jar > $REPOSITORY/nohup.out 2>&1 &
 
 #nohup java -jar /abc/ddd > $REPOSITORY/nohup.out 2>&1 &
 
