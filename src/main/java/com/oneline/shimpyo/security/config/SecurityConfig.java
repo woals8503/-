439bce2d74eaa4e5e8ff2acc564de6e3d9efc8e7
woals8503 <woals6391@gmail.com>
2023-06-28 16:42:37 +0900

    cors 수정중

--- src/main/java/com/oneline/shimpyo/security/config/SecurityConfig.java
+++ src/main/java/com/oneline/shimpyo/security/config/SecurityConfig.java
@@ -134,12 +134,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS", "PUT","DELETE"));
-        configuration.addAllowedOrigin("http://shimpyo-api.p-e.kr:8081");
-        configuration.addAllowedOrigin("http://localhost:8081");
-        configuration.addAllowedOrigin("http://shimpyo.o.p-e.kr:8081");
-        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
